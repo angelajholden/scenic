79bac936e8b97fa51f085c4067a037298b7d40db
Angela J Holden <angelajholden@gmail.com>
2026-02-15 11:51:51 -0600

    sharing js

--- script.js
+++ script.js
@@ -61,9 +61,49 @@ function form() {
 	}
 }
 
+function printButton() {
+	const print = document.querySelector(".print_icon");
+	if (print) {
+		print.addEventListener("click", () => {
+			console.log("clicked");
+			window.print();
+		});
+	}
+}
+
+function sharingIcons() {
+	// get the url
+	const URL = window.location.href;
+	const encodeUrl = encodeURI(URL);
+
+	// get the featured img
+	const image = document.querySelector("img");
+	if (!image) return;
+	const imgEncode = encodeURI(image.src);
+
+	// get the title
+	const title = document.querySelector(".primary_heading");
+	if (!title) return;
+	const titleEncode = encodeURI(title.textContent);
+
+	const facebook = document.querySelector(".facebook");
+	const x = document.querySelector(".x");
+	const pinterest = document.querySelector(".pinterest");
+	const linkedin = document.querySelector(".linkedin");
+	const email = document.querySelector(".email");
+
+	facebook.href = `https://www.facebook.com/sharer/sharer.php?u=${encodeUrl}`;
+	x.href = `https://twitter.com/intent/tweet?text=${titleEncode}%20${encodeUrl}`;
+	pinterest.href = `https://www.pinterest.com/pin/create/button/?url=${encodeUrl}&media=${imgEncode}&description=${titleEncode}`;
+	linkedin.href = `https://www.linkedin.com/shareArticle?mini=true&url=${encodeUrl}`;
+	email.href = `mailto:?subject=${titleEncode}&body=${titleEncode}%20${encodeUrl}`;
+}
+
 document.addEventListener("DOMContentLoaded", () => {
 	copyright();
 	menuToggle();
 	escapeToggle();
 	form();
+	printButton();
+	sharingIcons();
 });
